214ce1b88b0fa0f02750cefbd1f3556cff7bbf05
Som Visai <visai.s@mangotracking.com>
2020-03-31 12:40:50 +0800

    Update new line messenge 06

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function handleEvent(event) {
     var _geocoder;
 
     geocoder.reverse(result[0].lat,result[0].lng).then(_geocoder => {
-      var _lineMessage ='Auto Line === Alert ' + result[0].event_desc + ' === \n' + 
+      var _lineMessage ='=== Alert ' + result[0].event_desc + ' === \n' + 
               'On ' + _timezone[0] + '-' + _timezone[1] + '-' + _timezone[2] + ' At ' + _timezone[3] + ':' + _timezone[4] + '\n' +
               result[0].name + '\n' +
               'Location address is ' + _geocoder.display_name + '\n'+
